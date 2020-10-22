dae989aa34e581ba7913e20c292d12338ffc1afd
Sinem ertem <wolfsinem@mbp-van-sinem.home>
2020-10-22 17:52:23 +0200

    add route for tags generator

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ import sys
 
 sys.path.append('/Users/wolfsinem/product-tagging')
 from product_tagging.tags_generator import tokenized_list
+from deploy_tags_generator_text import tokenize_user_text_input
+
 
 N = 2000 
 
-# Load in our dataframe from the tags_generator file
+ # Load in our dataframe from the tags_generator file
 MODEL = tokenized_list()
-
 
 # Preprocessing 
 model_df = MODEL[:N]
@@ -21,7 +22,6 @@ target_variable = mlb.fit_transform(target_variable)
 # Open our classifier and vectorizer pickle files
 with open('text_classifier.pkl', 'rb') as training_model:
     model = pickle.load(training_model)
-
 with open('tfidfvectorizer.pkl', 'rb') as tfvectorizer:
     vectorizer = pickle.load(tfvectorizer)
 
@@ -41,6 +41,7 @@ def predict():
     application starts and create an API endpoint that takes input variables, 
     transforms them into the appropiate format and return predictions; set of tags.
     """
+
     if request.method == 'POST':
 
         user_input_string = request.form.values()
@@ -50,8 +51,34 @@ def predict():
         generated_tags = mlb.inverse_transform(label)
 
         # Output the generated tags by the machine learning model
-        return render_template("index.html", generated_tags = 'The set of generated tags are {}'.format(generated_tags))
+        return render_template("index.html", generated_tags = 'The set of predicted tags are {}'.format(generated_tags))
 
+
+# @app.route('/algorithm', methods = ['GET','POST'])
+# def generate():
+#     """serve main algorithm template """
+#     return render_template("algorithm.html")
+
+
+@app.route('/generated', methods=['POST', 'GET'])
+def generate_tags():
+    """This product generator doesn't use any machine learning to create tags 
+    based on the text input. It only uses the NLTK library.
+    """
+
+    if request.method == 'POST':
+        # TODO ask user how many tags they want to generate
+        tags_size = 20
+        user_input_string = request.form.values()
+        tags_set = tokenize_user_text_input(user_input_string, tags_size)
+
+        # Output the generated tags by the machine learning model
+        return render_template("algorithm.html", tags_set = 'The set of generated tags are {}'.format(tags_set))
     
+    return render_template("algorithm.html")
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
