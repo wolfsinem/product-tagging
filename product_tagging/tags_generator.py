d92fe5754e35b29d32656e422e3e4cf265cdb23c
Sinem ertem <wolfsinem@mbp-van-sinem.home>
2020-11-13 14:40:44 +0100

    export df to csv in new dir

--- product_tagging/tags_generator.py
+++ product_tagging/tags_generator.py
@@ -1,6 +1,7 @@
 # Data processing
 import pandas as pd
 import numpy as np
+import os
 
 # Data visualisation
 import matplotlib.pyplot as plt
@@ -18,10 +19,13 @@ def create_df(filepath='/Users/wolfsinem/product-tagging/data/flipkart_com-ecomm
     :param filepath: Path of chosen csv file.
     :type filepath: string
     """
+
     df = pd.read_csv((filepath))
+    
     model_df = df[['product_name','description']]
     pd.options.mode.chained_assignment = None 
     model_df['tags'] = ""
+    
     return model_df
 
 
@@ -33,6 +37,7 @@ def tokenize_string(sentence, size_tags=5):
     :param sentence: Text description of a product
     :type sentence: string
     """
+
     tokenizer = nltk.RegexpTokenizer(r"\w+")
     new_words = tokenizer.tokenize(sentence)
     new_words = [token.lower() for token in new_words]
@@ -44,6 +49,7 @@ def tokenize_string(sentence, size_tags=5):
 
     filter_tokens = [w for w in new_words if not w in stop_words and not w in manual_filtered_words]
     count_terms = Counter(filter_tokens).most_common(size_tags)
+    
     return [item[0] for item in count_terms]
 
 
@@ -51,8 +57,10 @@ def tokenized_list():
     """This function creates a nested list with all the tokenized descriptions
     and adds them to the dataframe.
     """
+
     model_df = create_df()
     token_lists = []
+
     for i in model_df['description']:
         token_lists.append(
             [x for x in tokenize_string(str((i))) if not any(c.isdigit() for c in x)]
@@ -64,9 +72,28 @@ def tokenized_list():
     model_df.dropna(inplace=True)
     
     return model_df
+
+
+def export_csv(df):
+    """This function will export the final df with the newly added 'tags' 
+    column to CSV. 
+    """
     
+    outname = 'tagged_products.csv'
+
+    outdir = './tagged_csv'
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+
+    fullname = os.path.join(outdir, outname)    
+    df.to_csv(fullname)
+
 
 if __name__ == "__main__":
+
+    # new dataframe with tags column 
     df_with_tags = tokenized_list()
-    print(df_with_tags.head())
+
+    # exporting it to CSV
+    export_csv(df=df_with_tags)
     
